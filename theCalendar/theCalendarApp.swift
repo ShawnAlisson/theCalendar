282b45438286fa22890b18972c8734bf87617a3e
ShaiiiaN <shayanalizadeh@outlook.com>
2022-11-08 02:08:44 +0330

    Initial Commit

---
+++ theCalendar/theCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  theCalendarApp.swift
+//  theCalendar
+//
+//  Created by Shayan Alizadeh on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct theCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
